1ef88113260b5aab6b65b17d2cb00fcb5cc68a19
Liew Eden <b2345dit@mdis.edu.my>
2024-07-08 16:00:44 +0800

    add javascript

--- script/room-details.js
+++ script/room-details.js
@@ -26,3 +26,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+function bookNow(roomType) {
+  window.location.href = `booking.html?roomType=${encodeURIComponent(roomType)}`;
+}
